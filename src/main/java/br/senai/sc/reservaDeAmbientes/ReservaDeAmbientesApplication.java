b2c539b2f897e18ed86d7d5985c7a7ddb84e7718
Felipe Luan Cipriani <felipeluan.12@hotmail.com>
2020-02-05 23:55:07 -0300

    Inicializando projeto

---
+++ src/main/java/br/senai/sc/reservaDeAmbientes/ReservaDeAmbientesApplication.java
@@ -0,0 +1,13 @@
+package br.senai.sc.reservaDeAmbientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaDeAmbientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaDeAmbientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senai/sc/reservaDeAmbientes/ReservaDeAmbientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.sc.reservaDeAmbientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaDeAmbientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
